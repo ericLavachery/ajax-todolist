11eb40ec43b7736296574b0a6e349610a4addd65
ericLavachery <eric.lavachery@gmail.com>
2018-07-24 12:10:55 +0200

    get and print, no record yet

---
+++ todo.js
@@ -0,0 +1,97 @@
+function Todo(task, who, dueDate) {
+    this.task = task;
+    this.who = who;
+    this.dueDate = dueDate;
+    this.done = false;
+}
+
+var todos = new Array();
+
+window.onload = init;
+
+function init() {
+    var submitButton = document.getElementById("submit");
+    submitButton.onclick = getFormData;
+    getTodoData();
+}
+
+function getTodoData() {
+    var request = new XMLHttpRequest();
+    request.open("GET", "todo.json");
+    request.onreadystatechange = function() {
+        if (this.readyState == this.DONE && this.status == 200) {
+            if (this.responseText) {
+                parseTodoItems(this.responseText);
+                addTodosToPage();
+            }
+            else {
+                console.log("Error: Data is empty");
+            }
+        }
+    };
+    request.send();
+}
+
+function parseTodoItems(todoJSON) {
+    if (todoJSON == null || todoJSON.trim() == "") {
+        return;
+    }
+    var todoArray = JSON.parse(todoJSON);
+    if (todoArray.length == 0) {
+        console.log("Error: the to-do list array is empty!");
+        return;
+    }
+    for (var i = 0; i < todoArray.length; i++) {
+        var todoItem = todoArray[i];
+        todos.push(todoItem);
+    }
+}
+
+function addTodosToPage() {
+    var ul = document.getElementById("todoList");
+    for (var i = 0; i < todos.length; i++) {
+        var todoItem = todos[i];
+        var li = document.createElement("li");
+        var kol = 'gris';
+        if (todoItem.dueDate == 'today') {
+            var kol = 'rouge';
+        }
+        li.innerHTML = '<span class="bleu">' + todoItem.who + " :</span> " + todoItem.task + ' <span class="' + kol + '">(' + todoItem.dueDate + ')</span>';
+        ul.appendChild(li);
+    }
+}
+
+function getFormData() {
+    var task = document.getElementById("task").value;
+    if (checkInputText(task, "Please enter a task")) return;
+
+    var who = document.getElementById("who").value;
+    if (checkInputText(who, "Please enter a person to do the task")) return;
+
+    var date = document.getElementById("dueDate").value;
+    if (checkInputText(date, "Please enter a due date")) return;
+
+    var todoItem = new Todo(task, who, date);
+    todos.push(todoItem);
+    addTodoToPage(todoItem);
+}
+
+function checkInputText(value, msg) {
+    if (value == null || value == "") {
+        alert(msg);
+        return true;
+    }
+    return false;
+}
+
+function addTodoToPage(todoItem) {
+    var ul = document.getElementById("todoList");
+    var li = document.createElement("li");
+    var kol = 'gris';
+    if (todoItem.dueDate == 'today') {
+        var kol = 'rouge';
+    }
+    li.innerHTML = '<span class="bleu">' + todoItem.who + " :</span> " + todoItem.task + ' <span class="' + kol + '">(' + todoItem.dueDate + ')</span>';
+    ul.appendChild(li);
+    document.forms[0].reset();
+}
